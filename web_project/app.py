d59d45916a79b42c31f3c50b4b023f4e29924475
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-06-28 11:49:42 +0300

    Update app.py

--- web_project/app.py
+++ web_project/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
+import re
 
 app = Flask(__name__)
 # חיבור לבסיס הנתונים
