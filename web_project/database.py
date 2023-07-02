6bca151103a09dc1f66eb9f7e61fa3c6e24d4955
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-02 09:45:09 +0300

    Update database.py

--- web_project/database.py
+++ web_project/database.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
+# יצירת חיבור למסד הנתונים
 def create_db_connection():
     try:
         connection = mysql.connector.connect(
             host='db',
-            port="3306",
+            port='3306',
             user='root',
             password='yotam',
             database='web_users'
@@ -14,36 +15,28 @@ def create_db_connection():
         print(f"Failed to connect to the database: {error}")
         return None
 
-
-def fetch_data(query, params=None):
-    try:
-        connection = create_db_connection()
-        if connection:
-            cursor = connection.cursor()
-            cursor.execute(query, params)
-            result = cursor.fetchone()
-            cursor.close()
-            connection.close()
-            return result
-        else:
-            return None
-    except mysql.connector.Error as error:
-        print(f"Failed to fetch data from the database: {error}")
+# פונקציה לביצוע שאילתות פשוטות והחזרת התוצאות
+def execute_query(query, params=None):
+    connection = create_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute(query, params)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result
+    else:
         return None
 
-
+# פונקציה להכנסת נתונים למסד הנתונים
 def insert_data(query, params=None):
-    try:
-        connection = create_db_connection()
-        if connection:
-            cursor = connection.cursor()
-            cursor.execute(query, params)
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return True
-        else:
-            return False
-    except mysql.connector.Error as error:
-        print(f"Failed to insert data into the database: {error}")
+    connection = create_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute(query, params)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return True
+    else:
         return False
