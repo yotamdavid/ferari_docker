ce3b689df83aad7ecc006afd46b5d87bfb010d14
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-02 10:08:20 +0300

    Update app.py

--- web_project/app.py
+++ web_project/app.py
@@ -11,7 +11,7 @@ def create_db_connection():
     try:
         connection = mysql.connector.connect(
             host='db',
-            port='3306',
+            port=3306,
             user='root',
             password='yotam',
             database='web_users'
