e3c6f303cb1bac8880aba83626b034ae59d1a038
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-04 09:44:13 +0300

    Update database.py

--- web_project/database.py
+++ web_project/database.py
@@ -7,7 +7,7 @@ def create_db_connection():
         port='3306',
         user='root',
         password='yotam',
-        database='users_web'
+        database='web_users'
     )
     return connection
 
