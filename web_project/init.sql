cfadddc078cef0ce38151545e931af3163d418d9
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-08 12:31:42 +0300

    Update init.sql

--- web_project/init.sql
+++ web_project/init.sql
@@ -1,6 +1,16 @@
-CREATE TABLE visits (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  visit_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- sql.ini
+
+-- יצירת מסד הנתונים
+CREATE DATABASE users;
+
+-- שימוש במסד הנתונים
+USE users;
+
+-- יצירת הטבלה לניהול משתמשים
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
 
 
